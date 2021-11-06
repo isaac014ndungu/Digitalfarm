e820ff5eb9883d4f9bd4976f155ae6b1d68fc4d8
isaac014ndungu <66169548+isaac014ndungu@users.noreply.github.com>
2021-11-06 12:31:23 +0300

    Add files via upload

---
+++ farm.js
@@ -0,0 +1,10 @@
+confirm("Would you like to view this page?");
+prompt("How old are you?");
+if (age<18){
+    aalert("You are not allowed to view this page");
+}
+var menuBtn = document.getElementById
+("menuBtn")
+var sideNav = document.getElementById
+("sideNav")
+var menu = document.getElementById("menus")
